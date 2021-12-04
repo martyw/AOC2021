b557029bc4f2db13d64124027f7fc9c942c8f118
= <=>
2021-12-04 21:10:30 +0100

    code clean up

--- day4.py
+++ day4.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from optparse import OptionParser
+from copy import deepcopy
+from time import time
 
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename", help="file with input data", metavar="FILE")
@@ -17,11 +19,12 @@ def bingo(matrix):
 		if status.count(True) == BOARD_SIZE:
 			res = True
 			break
-	for i in range(BOARD_SIZE):
-		status = [elt[1] for elt in column(matrix, i)]
-		if status.count(True) == BOARD_SIZE:
-			res = True
-			break
+	if not res:
+		for i in range(BOARD_SIZE):
+			status = [elt[1] for elt in column(matrix, i)]
+			if status.count(True) == BOARD_SIZE:
+				res = True
+				break
 
 	return res
 
@@ -37,13 +40,12 @@ def draw_part1(drawings, boards):
 		for board in boards:
 			update_board(drawings[0], board)
 			if bingo(board):
-				print("bingo", board)
 				is_bingo = True
 				break
 		if is_bingo:
 			break
 		drawings.pop(0)
-	return (drawings[0], board) if is_bingo else None
+	return (drawings[0], board) if is_bingo else (None, None)
 
 
 def calculate_score(board):
@@ -53,28 +55,6 @@ def calculate_score(board):
 			if not elt[1]:
 				notchecked_sum += elt[0]
 	return notchecked_sum
-
-def game_part1(filename):
-	try:
-		with open(options.filename, 'r') as f:
-			drawings = [int(x) for x in f.readline().split(',')]
-			_ = f.readline()
-			# filter empty lines
-			lines = list(filter(None, f.read().splitlines())) 
-			# list them in lists of five strings
-			boards_collection = [lines[x:x+BOARD_SIZE] for x in range(0, len(lines), BOARD_SIZE)]
-			# split each row in individual numbers
-			boards_collection = [[row.split() for row in rows] for rows in boards_collection]
-			# convert to [integer, draw status]
-			boards_collection = [[[[int(x), False] for x in row] for row in matrix] for matrix in boards_collection]
-			# play game part 1
-			lucky_draw, lucky_board = draw_part1(drawings, boards_collection)
-			if lucky_board:
-				score = calculate_score(lucky_board)
-				print(lucky_draw, score, lucky_draw * score)
-
-	except FileNotFoundError as e:
-		print(e)
 
 def draw_part2(drawings, boards, status_list):
 	last_bingo = len(status_list) - status_list.count(True)
@@ -90,14 +70,17 @@ def draw_part2(drawings, boards, status_list):
 			break
 				
 		drawings.pop(0)
-	return (drawings[0], boards[i]) if last_bingo == 0 else None
+	return (drawings[0], boards[i]) if last_bingo == 0 else (None, None)
 
-
-def game_part2(filename):
+if not options.filename:
+    parser.error('Filename not given')
+else:
 	try:
+		start_time = int(round(time() * 1000))
 		with open(options.filename, 'r') as f:
-			drawings = [int(x) for x in f.readline().split(',')]
+			drawings_part1 = [int(x) for x in f.readline().split(',')]
 			_ = f.readline()
+			drawings_part2 = deepcopy(drawings_part1)
 			# filter empty lines
 			lines = list(filter(None, f.read().splitlines())) 
 			# list them in lists of five strings
@@ -106,19 +89,18 @@ def game_part2(filename):
 			boards_collection = [[row.split() for row in rows] for rows in boards_collection]
 			# convert to [integer, draw status]
 			boards_collection = [[[[int(x), False] for x in row] for row in matrix] for matrix in boards_collection]
-			boards_status = [False] * len(boards_collection)
-			# play game part 2			
-			lucky_draw, lucky_board = draw_part2(drawings, boards_collection, boards_status)
+			# play game part 1
+			lucky_draw, lucky_board = draw_part1(drawings_part1, boards_collection)
 			if lucky_board:
 				score = calculate_score(lucky_board)
 				print(lucky_draw, score, lucky_draw * score)
 
+			# play game part 2			
+			boards_status = [False] * len(boards_collection)
+			lucky_draw, lucky_board = draw_part2(drawings_part2, boards_collection, boards_status)
+			if lucky_board:
+				score = calculate_score(lucky_board)
+				print(lucky_draw, score, lucky_draw * score)
+			print("### total run time is %s miliseconds" % (int(round(time() * 1000)) - start_time))
 	except FileNotFoundError as e:
 		print(e)
-
-
-if not options.filename:
-    parser.error('Filename not given')
-else:
-	game_part1(options.filename)
-	game_part2(options.filename)
