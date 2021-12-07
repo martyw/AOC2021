81a38487f4b4f6c90efc4548a3a76f51bc7cc6f3
martyw <martin.witte@gmail.com>
2021-12-07 14:37:50 +0100

    refactor

--- day7.py
+++ day7.py
@@ -11,14 +11,14 @@ def solver(data, distance_func):
 	return min([sum([distance_func(x, y) for y in data]) for x in range(min(data), max(data) + 1)])									
 								
 if not options.filename:
-    parser.error("Filename not given")
+	parser.error("Filename not given")
 else:
 	try:
 		start_time = time()
 		with open(options.filename, 'r') as f:
 			data = [int(i.strip()) for i in f.readline().split(',')]
 			print(solver(data, lambda x, y: abs(x - y)))
-			print(solver(data, lambda x, y: abs(x - y)*(abs(x - y) + 1)) // 2)
+			print(solver(data, lambda x, y: (abs(x - y)*(abs(x - y) + 1)) // 2))
 		print("*** Run time: {} ms".format((time() - start_time) * 1000))
 	except FileNotFoundError as e:
 		print(e)
