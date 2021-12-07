c53474f7c8244b22ccc1e246103a2b2d2e3df804
martyw <martin.witte@gmail.com>
2021-12-07 13:52:15 +0100

    refactored

--- day7.py
+++ day7.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from optparse import OptionParser
-
+from time import time
 
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename",
@@ -14,22 +14,20 @@ def distance_func_part2(dist):
 	return (dist*(dist + 1)) // 2
 
 def solve_puzzle(data, distance_func):		
-	fuel_at_position = {}
-	for position_x in range(max(data) + 1):
-			fuel_needed = 0
-			for position_y in data:
-				dist = abs(position_x - position_y)
-				fuel_needed += distance_func(dist)
-			fuel_at_position[position_x] = fuel_needed
-	return min(fuel_at_position.values())									
+	fuel_needed = {}
+	for position_x in range(min(data), max(data)):
+		fuel_needed[position_x] = sum([distance_func(abs(position_x - position_y)) for position_y in data])
+	return min(fuel_needed.values())									
 								
 if not options.filename:
     parser.error("Filename not given")
 else:
 	try:
+		start_time = time()
 		with open(options.filename, 'r') as f:
 			data = [int(i.strip()) for i in f.readline().split(',')]
 			print(solve_puzzle(data, distance_func_part1))
 			print(solve_puzzle(data, distance_func_part2))
+		print("*** Run time: {} ms".format((time() - start_time) * 1000))
 	except FileNotFoundError as e:
 		print(e)
