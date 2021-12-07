2cc827c724774dc86f3e5eea4dc70ed15df4d019
martyw <martin.witte@gmail.com>
2021-12-07 11:37:15 +0100

    refactor

--- day7.py
+++ day7.py
@@ -7,25 +7,21 @@ parser.add_option("-f", "--file", dest="filename",
                   help="file with input data", metavar="FILE")
 (options, args) = parser.parse_args()
 
-def part1(data):		
+def distance_func_part1(dist):
+	return dist
+
+def distance_func_part2(dist):
+	return (dist*(dist + 1)) // 2
+
+def solve_puzzle(data, distance_func):		
 	fuel_at_position = {}
 	for position_x in range(max(data) + 1):
 			fuel_needed = 0
 			for position_y in data:
-				dist = abs(position_x -position_y)
-				fuel_needed += dist
+				dist = abs(position_x - position_y)
+				fuel_needed += distance_func(dist)
 			fuel_at_position[position_x] = fuel_needed
 	return min(fuel_at_position.values())									
-
-def part2(data):
-	fuel_at_position = {}
-	for position_x in range(max(data)+1):
-			fuel_needed = 0
-			for position_y in data:
-				dist = abs(position_x -position_y)
-				fuel_needed += int((dist*(dist + 1))/2)
-			fuel_at_position[position_x] = fuel_needed
-	return min(fuel_at_position.values())	
 								
 if not options.filename:
     parser.error("Filename not given")
@@ -33,7 +29,7 @@ else:
 	try:
 		with open(options.filename, 'r') as f:
 			data = [int(i.strip()) for i in f.readline().split(',')]
-			print(part1(data))
-			print(part2(data))
+			print(solve_puzzle(data, distance_func_part1))
+			print(solve_puzzle(data, distance_func_part2))
 	except FileNotFoundError as e:
 		print(e)
