a087423ae1e5e8455265f525c1b672d917728543
= <=>
2021-12-09 22:34:58 +0100

    clean up

--- day9.py
+++ day9.py
@@ -8,17 +8,14 @@ parser.add_option("-f", "--file", dest="filename",
 (options, args) = parser.parse_args()
 
 def neighbours(point, dim_x, dim_y):
-	x = point[0]
-	y = point[1]
+	(x, y) = (point[0], point[1])
 	
 	res = [(x, y) for (x, y) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)] if 0 <= x < dim_x and 0 <= y < dim_y]
 	
 	return res
 
-
 def find_low_points(data):
-	dim_x = len(data)
-	dim_y = len(data[0])
+	(dim_x, dim_y) = (len(data), len(data[0]))
 
 	low_points = []
 
@@ -32,22 +29,18 @@ def find_low_points(data):
 	
 	return low_points
 	
-
 def part1(data):
 	risk_level = [data[i][j] + 1 for i, j in find_low_points(data)]
 
 	return sum(risk_level)
 							
 def find_basin_size(res, point, data, points_in_basin):
-	x = point[0]
-	y = point[1]
-	dim_x = len(data)
-	dim_y = len(data[0])
+	(x, y)  = (point[0], point[1])
+	(dim_x, dim_y) = (len(data), len(data[0]))
 
 	points_in_basin[x][y] = True
 	for n in neighbours((point[0], point[1]), dim_x, dim_y):
-		neighbour_x = n[0]
-		neighbour_y = n[1]
+		(neighbour_x, neighbour_y) = (n[0], n[1])
 		
 		if not points_in_basin[neighbour_x][neighbour_y] and data[x][y] <= data[neighbour_x][neighbour_y] < 9:
 			points_in_basin[neighbour_x][neighbour_y] = True
@@ -56,14 +49,11 @@ def find_basin_size(res, point, data, points_in_basin):
 	return res
 	
 def part2(data):
-	dim_x = len(data)
-	dim_y = len(data[0])
+	(dim_x, dim_y) = (len(data), len(data[0]))
 
 	basin_sizes = []
-	points_in_basin = [[False] * dim_y for i in range(dim_x)]
-
 	for i, j in find_low_points(data):
-		res = find_basin_size(1, (i,j), data, points_in_basin)
+		res = find_basin_size(1, (i,j), data, [[False] * dim_y for i in range(dim_x)])
 		basin_sizes.append(res)
 	
 	# multiply length of three biggest basins
