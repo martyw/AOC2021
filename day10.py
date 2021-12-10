a944deefeb0a0b5521ea3c3e917eb9a62857b64c
martyw <martin.witte@gmail.com>
2021-12-10 16:38:38 +0100

    refactor

--- day10.py
+++ day10.py
@@ -19,28 +19,16 @@ def parse_chunk(chunk, token_pairs):
 		else:
 			return ch
 	
-	return 0
+	return [token_pairs[ch] for ch in reversed(open_tokens)]
 
 def part1(data):
 	token_pairs = { '(': ')', '[': ']', '{':'}', '<': '>'}
 	score_map = {')': 3, ']': 57, '}': 1197, '>': 25137 }
 
 	ret = [ parse_chunk(line, token_pairs) for line in data ]
-	ret = [ score_map[i] for i in ret if i ]
+	ret = [ score_map[i] for i in ret if type(i) is str ]
 	
 	return sum(ret)
-
-def completion_chunk(chunk, token_pairs):
-	open_tokens = []
-	for ch in chunk:
-		if ch in token_pairs.keys():
-			open_tokens.append(ch)
-		elif token_pairs[open_tokens[-1]] == ch:
-			open_tokens.pop()
-		else:
-			pass
-	
-	return [token_pairs[ch] for ch in reversed(open_tokens)]
 
 def calculate_completion_score(li, score_map):
 	score = 0
@@ -53,10 +41,11 @@ def part2(data):
 	token_pairs = { '(': ')', '[': ']', '{':'}', '<': '>'}
 	completion_score = {')': 1, ']': 2, '}': 3, '>': 4 }
 	
-	not_corrupted_lines = [ line for line in data if not parse_chunk(line, token_pairs) ]
-	completion_lists = [completion_chunk(chunk, token_pairs) for chunk in not_corrupted_lines]
+	not_corrupted_lines = [ line for line in data if type(parse_chunk(line, token_pairs)) is list ]
+	completion_lists = [parse_chunk(chunk, token_pairs) for chunk in not_corrupted_lines]
 	completion_scores = [calculate_completion_score(li, completion_score) for li in completion_lists]
-	print(sorted(completion_scores)[len(completion_scores) // 2])
+	
+	return sorted(completion_scores)[len(completion_scores) // 2]
 										
 if not options.filename:
 	parser.error("Filename not given")
